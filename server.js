498a1b01249b2ab5ff179e301e05b436f99504ac
vraj dudhat08 <vrajdudhat611@gmail.com>
2025-07-07 10:40:09 +0530

    fix: add root route

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', orderRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Server is running ✅');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
